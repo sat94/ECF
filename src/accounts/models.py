63cd8f92af6aaa204ab781f6b13437a1d3b6fce7
sat94 <sultan77@live.fr>
2022-07-12 02:25:30 +0200

    firt commit

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,4 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class MyUser(AbstractUser):
-    pass
+    pseudo = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
+    nom = models.CharField(max_length=10)
+    prenom = models.CharField(max_length=10)
+    age = models.IntegerField(default=0)
+    email = models.EmailField()
+    adresse = models.CharField(max_length=200)
+    photo = models.ImageField()
+    partenaire = models.BooleanField(default=False)
+
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from telnetlib import AUTHENTICATION
+from django.contrib.auth import get_user_model, login, logout, authenticate
+from django.shortcuts import redirect, render
+from accounts.forms import SignupForm, inscription
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return render(request, 'index.html')
+    else:
+        form = SignupForm()
+
+    return render(request, "accounts/signup.html", {"form": form})
+
+   
+def inscrip(request):
+    form = inscription()
+
+    return render(request, "inscription.html",{"form" : form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
